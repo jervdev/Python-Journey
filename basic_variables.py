89be05256faba1b3a6aa22d9572f456dc89b5a64
Jervee <jerveemanangan@gmail.com>
2026-06-22 01:54:05 +0800

    Python basics: variables and math operators

---
+++ basic_variables.py
@@ -0,0 +1,9 @@
+x=10
+y=5
+z=8
+w = "this is an example of prompt programming"
+u = 'ab'
+
+print(x+y)
+print(y+z)
+print(u+w) 
---
+++ string_and_math_operators.py
@@ -0,0 +1,17 @@
+#Jervee Manangan
+#ICT-105
+x=1
+y=10
+z=100
+
+q = 'x'
+w = '*'
+e = '0'
+
+print(x+y)
+print(y*1000)
+print(1/1000)
+print(q+w)
+print(w+e)
+print(e*y)
+print(q*z, w*z)
