c9187623195659af013c1d0b659c750ef0aa691a
Jervee <jerveemanangan@gmail.com>
2026-06-22 22:25:14 +0800

    Escape sequences and ASCII

---
+++ escape_sequences_and_ascii.py
@@ -0,0 +1,18 @@
+quote = "\"HELLO WORLD!\""
+print(quote)
+print("\t")
+line = "2\n4\n6\n8\n10"
+print(line)
+print("\t")
+ast = "*\n**\n***\n****\n*****"
+print(ast)
+print("\t")
+rainbow = "\t\t\tGreen\n\t\tYellow\t\tBlue\n\tOrange\t\t\t\tIndigo\nRed\t\t\t\t\t\tViolet"
+print(rainbow)
+print("\t")
+#heart emoji
+print("\u2764")
+character = 'U'
+ascii_value = ord(character)
+print(f"The ASCII value of {character} is {ascii_value}")
+print(f"The character {character} is the ASCII value {ascii_value}")
