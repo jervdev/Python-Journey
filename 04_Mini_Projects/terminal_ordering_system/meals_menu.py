c19e8f250f67d30831006f00deafadd8b8d2342e
jervdev <jerveemanangan@gmail.com>
2026-07-08 22:14:46 +0800

    Add files via upload

---
+++ 04_Mini_Projects/terminal_ordering_system/meals_menu.py
@@ -0,0 +1,10 @@
+def meals():
+    global menu
+    print("---- Meals ----")
+    mealmenu = ["[A] Classic Jolly ", "[B] Jolly Time", "[C] Big Jolly", "[D] Jolly Meal"]
+    mealprice = [60, 70, 80, 90]
+    menu = mealprice
+    for i in mealmenu:
+        print(i)
+
+ 
